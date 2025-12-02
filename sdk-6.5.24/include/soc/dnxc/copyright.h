f36df01b03578db51e2bd8bcb47ab6c98f1d5f19
Vinolzy <43488051+Vinolzy@users.noreply.github.com>
2025-12-02 16:18:18 +0800

    Delete sdk-6.5.24 directory

--- sdk-6.5.24/include/soc/dnxc/copyright.h
+++
@@ -1,5 +0,0 @@
-/*
- * This license is set out in https://raw.githubusercontent.com/Broadcom-Network-Switching-Software/OpenBCM/master/Legal/LICENSE file.
- * 
- * Copyright 2007-2021 Broadcom Inc. All rights reserved.
- */
--- sdk-6.5.24/src/soc/phy/wcmod/src/subdir.mk
+++
@@ -1,8 +0,0 @@
-#
-# $Id: Makefile,v 1.3 Broadcom SDK $
-# This license is set out in https://raw.githubusercontent.com/Broadcom-Network-Switching-Software/OpenBCM/master/Legal/LICENSE file.
-# 
-# Copyright 2007-2021 Broadcom Inc. All rights reserved.
-#
-subdirs += wcmod/src
-
