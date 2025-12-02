ac2321a356f3827430ef0912c5806d0f8171e814
Vinolzy <43488051+Vinolzy@users.noreply.github.com>
2025-12-02 16:17:47 +0800

    Delete sdk-6.5.16 directory

--- sdk-6.5.16/libs/phymod/include/phymod/phymod_doc.h
+++
@@ -1,7 +0,0 @@
-/*
- * This license is set out in https://raw.githubusercontent.com/Broadcom-Network-Switching-Software/OpenBCM/master/Legal/LICENSE file.
- * 
- * Copyright 2007-2019 Broadcom Inc. All rights reserved.
- * 
-*/
-
